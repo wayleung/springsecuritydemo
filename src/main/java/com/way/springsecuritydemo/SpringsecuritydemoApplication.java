4bb6d21095d49e6d36f9883f41451d4a0b505386
wayleung <610686949@qq.com>
2018-11-11 15:52:21 +0800

    first commit

---
+++ src/main/java/com/way/springsecuritydemo/SpringsecuritydemoApplication.java
@@ -0,0 +1,12 @@
+package com.way.springsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritydemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecuritydemoApplication.class, args);
+    }
+}
